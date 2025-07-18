d635754677e264c52451494b64f8b7b28394731b
Rakesh Bhandary <rakeshbhandary08@gmail.com>
2025-07-18 16:53:09 +0530

    Payment added

--- first.js
+++ first.js
@@ -1,6 +1,10 @@
 const button="Added a button";
-
 console.log(button);
+
 
 const Login="Login Page Added";
 console.log(Login);
+
+
+const Payment="Integrated the Payment gateway"
+console.log(Payment);
