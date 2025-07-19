1ae03b2566e35aa94205e8b6f724b59ae8f0f056
Rakesh Bhandary <rakeshbhandary08@gmail.com>
2025-07-19 16:25:46 +0530

    GitHub Learningggg

--- first.js
+++ first.js
@@ -1,13 +1,14 @@
-const button="Added a button";
+const button = "Added a button";
 console.log(button);
 
-
-const Login="Login Page Added";
+const Login = "Login Page Added";
 console.log(Login);
 
-
-const Payment="Integrated the Payment gateway"
+const Payment = "Integrated the Payment gateway";
 console.log(Payment);
 
-const upi="Integrated the UPI also"
-console.log(upi)
+const upi = "Integrated the UPI also";
+console.log(upi);
+
+let gitHub="GitHub Learning";
+console.log(gitHub);
